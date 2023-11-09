06484eb94573962c892fd56b8f77790bc77b7f25
Monika <monika.zemankiewicz.2015@gmail.com>
2023-11-09 15:23:10 +0100

    First commit

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section>
+      <h1 className="text-white"> Hello, I'm Monika</h1>
+    </section>
+  );
+};
+
+export default Hero;
