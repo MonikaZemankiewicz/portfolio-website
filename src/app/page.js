8919289a7bd1fb3c8cb94b1f2dd635c943ea21cc
Monika <monika.zemankiewicz.2015@gmail.com>
2023-11-14 04:55:30 +0100

    scrolling header - cokolwiek

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import Skills from "./components/Skills";
+import ScrollingHeader from "./components/ScrollingHeader";
 
 export default function Home() {
   return (
@@ -10,10 +11,11 @@ export default function Home() {
       <Navbar></Navbar>
       <div className="container mt-24 mx-auto px-12 py-4">
         <Hero></Hero>
-        <hr className="mt-24 mb-12 "></hr>
+        {/* <hr className="mt-24 mb-12 "></hr> */}
         <AboutSection></AboutSection>
-        <hr className="mt-24 mb-12"></hr>
+        {/* <hr className="mt-24 mb-12"></hr> */}
         <Skills></Skills>
+        <ScrollingHeader></ScrollingHeader>
       </div>
     </main>
   );
