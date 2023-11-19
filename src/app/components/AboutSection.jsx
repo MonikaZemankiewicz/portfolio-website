5589351f9eb9b9924765c6d6c7d01a82a25ea380
Monika <monika.zemankiewicz.2015@gmail.com>
2023-11-19 23:20:32 +0100

    lava lamp

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -67,7 +67,7 @@ const AboutSection = () => {
   };
 
   return (
-    <section className="text-white">
+    <section className="text-white bg-slate-400">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16<">
         <Image
           alt="about image"
