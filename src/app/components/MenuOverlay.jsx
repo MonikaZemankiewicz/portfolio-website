f7c56568d7fcf63913d183139f4f667e640c947c
Monika <monika.zemankiewicz.2015@gmail.com>
2024-01-05 19:12:42 +0100

    changed sidebar alignment

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -4,7 +4,7 @@ import SocialIcons from "./SocialIcons";
 
 const MenuOverlay = ({ links }) => {
   return (
-    <div className="flex flex-col py-8 gap-6 items-center justify-center min-h-screen">
+    <div className="flex flex-col py-32 gap-6 items-center min-h-screen">
       {links.map((link, index) => (
         <NavbarLink key={index} href={link.href} text={link.text} />
       ))}
